7dfb672b5782bed1b45f5ab6446002ea2dc427b3
Gilvane Amaro <gilvane.amaro.jr@gmail.com>
2023-10-15 17:24:03 -0300

    added tasks constraints and findByIdUser

--- src/main/java/br/com/gilvaneamaro/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/gilvaneamaro/todolist/task/ITaskRepository.java
@@ -2,8 +2,10 @@ package br.com.gilvaneamaro.todolist.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-
+    List<TaskModel> findByIdUser(UUID idUser);
 }
